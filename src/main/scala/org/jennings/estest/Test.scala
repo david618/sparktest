c0a5e4a33f320de7348d1f0a42fa7b8ba3dd22d9
David Jennings <djennings@esri.com>
2017-11-06 07:45:23 -0600

    Initial commit

---
+++ src/main/scala/org/jennings/estest/Test.scala
@@ -0,0 +1,8 @@
+package org.jennings.estest
+
+/**
+  * Created by david on 11/4/17.
+  */
+object Test {
+  def main(args: Array[String]) = println("Hello world!")
+}
---
+++ src/main/scala/org/jennings/estest/Test2.scala
@@ -0,0 +1,8 @@
+package org.jennings.estest
+
+/**
+  * Created by david on 11/4/17.
+  */
+object Test2 extends App {
+  println("Hello world!")
+}
